9e9a8a4925878be02beaba779fb8f150822fd142
GlobalisTech <gist.globalistech@gmail.com>
2025-02-13 17:20:26 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -170,7 +170,7 @@ def create_performance_chart(data):
             title="Stock Symbol",
             title_font=dict(color="black"),  # Sets "Stock Symbol" font color to black
             tickangle=270,  # Keeps x-axis labels horizontal
-            tickfont=dict(size=14, color="black")
+            tickfont=dict(size=14, color="blue")
         ),
         yaxis_title="ROI (%)",
         template="plotly_dark",
